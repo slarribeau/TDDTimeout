e886ef3fe8d31f9f7063db098cf284a8575d42f9
Scott Larribeau <slarribeau@gmail.com>
2018-02-16 13:17:54 -0800

    Add factorial tests and sleep logic

--- src/main/Timeout.java
+++ src/main/Timeout.java
@@ -1,9 +1,25 @@
 package main;
 
+import java.math.BigInteger;
+
+import static java.lang.Thread.*;
+
 public class Timeout {
-    public void doWorkShort() {
-        // TODO Auto-generated method stub
+
+    public void simulateWork() throws InterruptedException {
+        sleep(2000);
     }
+
+    public static BigInteger factorial(BigInteger number) {
+        BigInteger result = BigInteger.valueOf(1);
+
+        for (long factor = 2; factor <= number.longValue(); factor++) {
+            result = result.multiply(BigInteger.valueOf(factor));
+        }
+
+        return result;
+    }
+
 
     public void doWorkInfinity() {
         while(true) {
--- src/test/TimeoutTest.java
+++ src/test/TimeoutTest.java
@@ -3,17 +3,63 @@ import static org.junit.Assert.*;
 import main.Timeout;
 import org.junit.Test;
 
-public class TimeoutTest {
+import java.math.BigInteger;
+import java.sql.Time;
 
-    @Test(timeout = 1000)
-    public void testShort() {
-        Timeout t = new Timeout();
-        t.doWorkShort();
-    }
+public class TimeoutTest {
 
     @Test(timeout = 1000)
     public void testInfinity() {
         Timeout t = new Timeout();
         t.doWorkInfinity();
+    }
+
+    @Test
+    public void getTinyFactorialNoTimeout() {
+        Timeout t = new Timeout();
+        BigInteger n = BigInteger.valueOf(4);
+        BigInteger r = t.factorial(n);
+        assertEquals(BigInteger.valueOf(24), r);
+    }
+
+    @Test
+    public void getSmallFactorialNoTimeout() {
+        Timeout t = new Timeout();
+        BigInteger n = BigInteger.valueOf(128);
+        BigInteger r = t.factorial(n);
+    }
+
+    @Test (timeout=1)
+    public void getSmallFactorialWithTimeout() {
+        Timeout t = new Timeout();
+        BigInteger n = BigInteger.valueOf(64);
+        BigInteger r = t.factorial(n);
+    }
+    @Test
+    public void getBigFactorialNoTimeout() {
+        Timeout t = new Timeout();
+        BigInteger n = BigInteger.valueOf(100000);
+        BigInteger r = t.factorial(n);
+        System.out.println(r);
+    }
+
+    @Test(timeout=4000)
+    public void getBigFactorialWithTimeout() {
+        Timeout t = new Timeout();
+        BigInteger n = BigInteger.valueOf(100000);
+        BigInteger r = t.factorial(n);
+        System.out.println(r);
+    }
+
+    @Test (timeout=1000)
+    public void testSleep() throws InterruptedException {
+        Timeout t = new Timeout();
+        t.simulateWork();
+    }
+
+    @Test ()
+    public void testSleepNoTimeout() throws InterruptedException {
+        Timeout t = new Timeout();
+        t.simulateWork();
     }
 }
